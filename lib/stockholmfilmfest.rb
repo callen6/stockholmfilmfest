00a5858bf098c047c82f98c592c8c0a2877abb0e
Cori Allen <coriallen@gmail.com>
2013-11-25 16:04:05 -0500

    Added initialize method and started on get method.

--- lib/stockholmfilmfest.rb
+++ lib/stockholmfilmfest.rb
@@ -3,5 +3,16 @@ require "stockholmfilmfest/version"
 module Stockholmfilmfest
   class Client
 
+  	attr_accessor :url
+  	def initialize(url)
+  		@url = url
+  	end
+
+  	def get
+
+  	end
+
+
+
   end
 end
--- spec/stockholmfilmfest/client_spec.rb
+++ spec/stockholmfilmfest/client_spec.rb
@@ -2,5 +2,15 @@ require_relative '../spec_helper'
 require 'Stockholmfilmfest'
 
 describe Stockholmfilmfest::Client do 
+	it "should have a URL endpoint" do 
+		cl = Stockholmfilmfest::Client.new('http://google.com')
+		cl.should respond_to :url
+	end
+
+	it "should make a get request" do
+		cl = Stockholmfilmfest::Client.new('http://google.com')
+		cl.get.should == "foooooooo"
+	end
+
 
 end
