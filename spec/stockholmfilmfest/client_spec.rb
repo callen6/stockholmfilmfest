797ea49127b53682d5ded17d4b0eaa13aeda78cd
Cori Allen <coriallen@gmail.com>
2013-11-26 09:41:45 -0500

    Add before do into tests to dry up code.

--- spec/stockholmfilmfest/client_spec.rb
+++ spec/stockholmfilmfest/client_spec.rb
@@ -3,36 +3,33 @@ require 'stockholmfilmfest'
 
 describe Stockholmfilmfest::Client do 
 	# set up before do that creates a client
-
+	before(:each) do
+		@cllient = Stockholmfilmfest::Client.new('http://api.stockholmfilmfestival.se/v1/films/film/film_id/3/format/json/API-Key/M61ps8d8v2QGXl2K0ccQwk4EbEfl0a9JVwwCuN0f/')
+	end
 
 
 	it "should have a URL endpoint" do 
-		cl = Stockholmfilmfest::Client.new('http://api.stockholmfilmfestival.se/v1/films/film/film_id/3/format/json/API-Key/M61ps8d8v2QGXl2K0ccQwk4EbEfl0a9JVwwCuN0f/')
-		cl.should respond_to :url
+		@client.should respond_to :url
 	end
 
 	it "should make a get request" do
-		cl = Stockholmfilmfest::Client.new('http://api.stockholmfilmfestival.se/v1/films/film/film_id/3/format/json/API-Key/M61ps8d8v2QGXl2K0ccQwk4EbEfl0a9JVwwCuN0f/')
-		response = cl.get
+		response = @client.get
 		json_response = JSON.parse(response)
 		json_response["filmId"].should eq "3"
 	end
 
-	it "should get the filmId" do 
-		cl = Stockholmfilmfest::Client.new('http://api.stockholmfilmfestival.se/v1/films/film/film_id/3/format/json/API-Key/M61ps8d8v2QGXl2K0ccQwk4EbEfl0a9JVwwCuN0f/')
-		response = cl.get_filmId
+	it "should get the filmId" do
+		response = @client.get_filmId
 		response.should eq "3"
 	end
 
 	it "should get the english name of the film" do
-		cl = Stockholmfilmfest::Client.new('http://api.stockholmfilmfestival.se/v1/films/film/film_id/3/format/json/API-Key/M61ps8d8v2QGXl2K0ccQwk4EbEfl0a9JVwwCuN0f/')
-		response = cl.get_filmName_en	
+		response = @client.get_filmName_en	
 		response.should eq "Outside Love"
 	end
 
 	it "should get the youtube id of the film" do
-		cl = Stockholmfilmfest::Client.new('http://api.stockholmfilmfestival.se/v1/films/film/film_id/3/format/json/API-Key/M61ps8d8v2QGXl2K0ccQwk4EbEfl0a9JVwwCuN0f/')
-		response = cl.get_filmYoutubeId
+		response = @client.get_filmYoutubeId
 		response.should eq ""
 	end
 end
